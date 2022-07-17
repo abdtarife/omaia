94ef8082ae17290cfb8e2640033271b9d614dd44
abdtarife <abdtarife699@gmail.com>
2022-07-17 12:25:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,126 @@
+  // Other important pens.
+    // Map: https://codepen.io/themustafaomar/pen/ZEGJeZq
+    // Dashboard: https://codepen.io/themustafaomar/pen/jLMPKm
+
+    let dropdowns = document.querySelectorAll('.navbar .dropdown-toggler')
+    let dropdownIsOpen = false
+
+    // Handle dropdown menues
+    if (dropdowns.length) {
+      // Usually I don't recommend doing this (adding many event listeners to elements have the same handler)
+      // Instead use event delegation: https://javascript.info/event-delegation
+      // Why: https://gomakethings.com/why-event-delegation-is-a-better-way-to-listen-for-events-in-vanilla-js
+      // But since we only have two dropdowns, no problem with that. 
+      dropdowns.forEach((dropdown) => {
+        dropdown.addEventListener('click', (event) => {
+          let target = document.querySelector(`#${event.target.dataset.dropdown}`)
+
+          if (target) {
+            if (target.classList.contains('show')) {
+              target.classList.remove('show')
+              dropdownIsOpen = false
+            } else {
+              target.classList.add('show')
+              dropdownIsOpen = true
+            }
+          }
+        })
+      })
+    }
+
+    // Handle closing dropdowns if a user clicked the body
+    window.addEventListener('mouseup', (event) => {
+      if (dropdownIsOpen) {
+        dropdowns.forEach((dropdownButton) => {
+          let dropdown = document.querySelector(`#${dropdownButton.dataset.dropdown}`)
+          let targetIsDropdown = dropdown == event.target
+
+          if (dropdownButton == event.target) {
+            return
+          }
+
+          if ((!targetIsDropdown) && (!dropdown.contains(event.target))) {
+            dropdown.classList.remove('show')
+          }
+        })
+      }
+    })
+
+    // Open links in mobiles
+    function handleSmallScreens() {
+      document.querySelector('.navbar-toggler')
+        .addEventListener('click', () => {
+          let navbarMenu = document.querySelector('.navbar-menu')
+
+          if (navbarMenu.style.display === 'flex') {
+            navbarMenu.style.display = 'none'
+            return
+          }
+
+          navbarMenu.style.display = 'flex'
+        })
+    }
+
+    handleSmallScreens()
+
+
+
+
+
+    $('.slider').slick({
+        centerMode: true,
+        slidesToShow: 5,
+        dots: true,
+        arrows: false,
+
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
+
+
+      // Get the modal 
+
+$(document).ready(function() {
+
+  // required elements
+  var imgPopup = $('.img-popup');
+  var imgCont  = $('.container__img-holder');
+  var popupImage = $('.img-popup img');
+  var closeBtn = $('.close-btn');
+
+  // handle events
+  imgCont.on('click', function() {
+    var img_src = $(this).children('img').attr('src');
+    imgPopup.children('img').attr('src', img_src);
+    imgPopup.addClass('opened');
+  });
+
+  $(imgPopup, closeBtn).on('click', function() {
+    imgPopup.removeClass('opened');
+    imgPopup.children('img').attr('src', '');
+  });
+
+  popupImage.on('click', function(e) {
+    e.stopPropagation();
+  });
+  
+});
